bebbabc7f8cdb9f527b4b2317a81e34bd93369c5
Ebubeigbo <igbo2805@gmail.com>
2023-08-29 11:34:11 +0100

    Updates

---
+++ precache-manifest.78a9fe1079ac5abf2eb283a0a825ee34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7655d5d0c4320226f74c",
+    "url": "/trvl/static/css/main.780eca9b.chunk.css"
+  },
+  {
+    "revision": "7655d5d0c4320226f74c",
+    "url": "/trvl/static/js/main.7655d5d0.chunk.js"
+  },
+  {
+    "revision": "5e625a6b76d03f5b1d69",
+    "url": "/trvl/static/js/1.5e625a6b.chunk.js"
+  },
+  {
+    "revision": "020ecae15337f509e139",
+    "url": "/trvl/static/js/runtime~main.020ecae1.js"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/trvl/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "17a585559880087d9f00d295d69ad34b",
+    "url": "/trvl/static/media/img-2.17a58555.jpg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/trvl/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "3cb2fa2c0a0b936a1d23bb3bf139a120",
+    "url": "/trvl/static/media/video-2.3cb2fa2c.mp4"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/trvl/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/trvl/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "9a7e72d30df7265e1b99fcbb73d2c147",
+    "url": "/trvl/static/media/img-9.9a7e72d3.jpg"
+  },
+  {
+    "revision": "b7e2396879ee74ce0bc1a0034a379713",
+    "url": "/trvl/index.html"
+  }
+];
